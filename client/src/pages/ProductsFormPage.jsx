ed60adbb1821cb7ed8eb66882a20ab1fbd917b58
ender <ender.1895.perez@gmail.com>
2023-08-16 19:57:12 -0500

    intento de subir archivo en agregar producto

--- client/src/pages/ProductsFormPage.jsx
+++ client/src/pages/ProductsFormPage.jsx
@@ -1,54 +1,57 @@
 import { useForm } from "react-hook-form";
 import { useProducts } from "../context/ProductsContext";
 import { useNavigate } from "react-router-dom";
-import "../style/preguntas.css"
+import "../style/preguntas.css";
+import { storage } from "../firebase/config";
+import { useState } from "react";
 
 function ProductsFormPage() {
+  const { register, handleSubmit, setValue } = useForm();
+  const { createProduct } = useProducts();
+  const navigate = useNavigate();
+  const [file, setFile] = useState(null);
 
-    const { register, handleSubmit } = useForm();
-    const {createProduct}= useProducts();
+  const escucharFile = (e) => {
+    const selectFile = e.target.files[0];
+    setFile(selectFile);
+  };
 
-    const navigate= useNavigate();
+  const guardarArchivo = async () => {
+    if (file) {
+      const storageRef = storage.ref();
+      const fileRef = storageRef.child(file.name);
 
-   const  onSubmit= handleSubmit((data) => {
-      createProduct(data);
-      navigate("/products");
-    })
- 
+      try {
+        await fileRef.put(file);
+        const url = await fileRef.getDownloadURL();
+        setValue("url", url); // Establecer el valor del campo "url" con la URL de la imagen
+      } catch (error) {
+        console.error("Error al subir el archivo:", error);
+      }
+    }
+  };
+
+  const onSubmit = handleSubmit((data) => {
+    createProduct(data);
+    navigate("/products");
+  });
+
   return (
     <div className="my-form">
       <div className="mb-3">
-
-      <form onSubmit={ onSubmit}>
-
-        <input className="form-control" type="text" placeholder="title" {...register("title")} autoFocus />
-
-        <input className="form-control" type="number" placeholder="price" {...register("price")} />
-
-        <input className="form-control" type="text" placeholder="description" {...register("description")} />
-
-        <input className="form-control" type="text" placeholder="category" {...register("category")} />
-
-        <input className="form-control" type="text" placeholder="url" {...register("url")} />
-        
-
-
-
-        {/* ACAAAAA VA A IR LA IMAGEN CON FIREBASE */}
-
-
-
-        
-
-        <button className="btn btn-primary">Agregar Producto</button>
-
-
-
-      </form>
+        <form onSubmit={onSubmit}>
+          <input className="form-control" type="text" placeholder="title" {...register("title")} autoFocus />
+          <input className="form-control" type="number" placeholder="price" {...register("price")} />
+          <input className="form-control" type="text" placeholder="description" {...register("description")} />
+          <input className="form-control" type="text" placeholder="category" {...register("category")} />
+          <input type="file" onChange={escucharFile} />
+          <button className="btn btn-primary" onClick={guardarArchivo}>
+            Subir Imagen y Agregar Producto
+          </button>
+        </form>
       </div>
-
     </div>
-  )
+  );
 }
 
 export default ProductsFormPage
